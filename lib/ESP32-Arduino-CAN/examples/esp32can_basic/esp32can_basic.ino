42fbb1555b082bc30f8b408a03f3d2cf4ae188d9
LiTang <litang0617@outlook.com>
2023-05-28 00:24:07 +0800

    update failed

--- lib/ESP32-Arduino-CAN/examples/esp32can_basic/esp32can_basic.ino
+++ lib/ESP32-Arduino-CAN/examples/esp32can_basic/esp32can_basic.ino
@@ -10,9 +10,9 @@ const int rx_queue_size = 10;       // Receive Queue size
 void setup() {
   Serial.begin(115200);
   Serial.println("Basic Demo - ESP32-Arduino-CAN");
-  CAN_cfg.speed = CAN_SPEED_1000KBPS;
-  CAN_cfg.tx_pin_id = GPIO_NUM_18;
-  CAN_cfg.rx_pin_id = GPIO_NUM_5;
+  CAN_cfg.speed = CAN_SPEED_125KBPS;
+  CAN_cfg.tx_pin_id = GPIO_NUM_5;
+  CAN_cfg.rx_pin_id = GPIO_NUM_4;
   CAN_cfg.rx_queue = xQueueCreate(rx_queue_size, sizeof(CAN_frame_t));
   // Init CAN Module
   ESP32Can.CANInit();
@@ -21,8 +21,31 @@ void setup() {
 void loop() {
 
   CAN_frame_t rx_frame;
+
   unsigned long currentMillis = millis();
 
+  // Receive next CAN frame from queue
+  if (xQueueReceive(CAN_cfg.rx_queue, &rx_frame, 3 * portTICK_PERIOD_MS) == pdTRUE) {
+
+    if (rx_frame.FIR.B.FF == CAN_frame_std) {
+      printf("New standard frame");
+    }
+    else {
+      printf("New extended frame");
+    }
+
+    if (rx_frame.FIR.B.RTR == CAN_RTR) {
+      printf(" RTR from 0x%08X, DLC %d\r\n", rx_frame.MsgID,  rx_frame.FIR.B.DLC);
+    }
+    else {
+      printf(" from 0x%08X, DLC %d, Data ", rx_frame.MsgID,  rx_frame.FIR.B.DLC);
+      for (int i = 0; i < rx_frame.FIR.B.DLC; i++) {
+        printf("0x%02X ", rx_frame.data.u8[i]);
+      }
+      printf("\n");
+    }
+  }
+  // Send CAN Message
   if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
     CAN_frame_t tx_frame;
@@ -38,32 +61,5 @@ void loop() {
     tx_frame.data.u8[6] = 0x06;
     tx_frame.data.u8[7] = 0x07;
     ESP32Can.CANWriteFrame(&tx_frame);
-    Serial.println("Sent\n");
   }
-
-  
-
-  // // Receive next CAN frame from queue
-  // if (xQueueReceive(CAN_cfg.rx_queue, &rx_frame, 3 * portTICK_PERIOD_MS) == pdTRUE) {
-
-  //   if (rx_frame.FIR.B.FF == CAN_frame_std) {
-  //     printf("New standard frame");
-  //   }
-  //   else {
-  //     printf("New extended frame");
-  //   }
-
-  //   if (rx_frame.FIR.B.RTR == CAN_RTR) {
-  //     printf(" RTR from 0x%08X, DLC %d\r\n", rx_frame.MsgID,  rx_frame.FIR.B.DLC);
-  //   }
-  //   else {
-  //     printf(" from 0x%08X, DLC %d, Data ", rx_frame.MsgID,  rx_frame.FIR.B.DLC);
-  //     for (int i = 0; i < rx_frame.FIR.B.DLC; i++) {
-  //       printf("0x%02X ", rx_frame.data.u8[i]);
-  //     }
-  //     printf("\n");
-  //   }
-  // }
-  // Send CAN Message
-  
 }
